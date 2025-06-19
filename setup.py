c7eeab18e62055fae17e11d3f1b9f55ee3073fb2
Sarah <sbetti@stsci.edu>
2025-06-19 15:01:59 -0400

    fixing names

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="specfit", 
+setup(name="bdfit", 
         version = "1.0",
         author = "Sarah Betti",
         author_email = "sbetti@stsci.edu",
-        url = "https://github.com/sbetti22/specfit",
+        url = "https://github.com/sbetti22/bdfit",
         packages = find_packages(),
         )
