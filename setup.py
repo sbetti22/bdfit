514b5c07919b82a3722db9e3d4e5ab3707fd9685
Sarah <sbetti@stsci.edu>
2025-06-19 15:57:03 -0400

    working version

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='bdfit',
-    version='1.0',
-    description='spectral fitting routine for low resolution brown dwarf NIR spectra using the species package',
-    url='https://github.com/sbetti22/bdfit',
-    author='Sarah Betti',
-    author_email='sbetti@stsci.edu',
-    license='MIT',
-    classifiers=[
+setup(name="bdfit", 
+        version = "1.0",
+        description='spectral fitting routine for low resolution brown dwarf NIR spectra using the species package',
+        author = "Sarah Betti",
+        author_email = "sbetti@stsci.edu",
+        license='MIT',
+        url = "https://github.com/sbetti22/bdfit",
+        classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research ',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License'
-    ],
-    keywords='NIR spectral fitting',
-    packages=find_packages(),
-    install_requires=[
+        ],
+        keywords='NIR spectral fitting',
+        packages = find_packages(exclude=['example']),
+        install_requires=[
         'numpy', 'scipy', 'astropy', 'matplotlib', 
-    ],
-    include_package_data=True,
-    zip_safe=False
-)
+        ], 
+        py_modules=['bdfit']
+        )
