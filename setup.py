1857eeed490b1337d2ebcb3dc1ec205e3ab62838
Sarah <sbetti@stsci.edu>
2025-06-19 14:59:17 -0400

    first version

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="specfit", 
+        version = "1.0",
+        author = "Sarah Betti",
+        author_email = "sbetti@stsci.edu",
+        url = "https://github.com/sbetti22/specfit",
+        packages = find_packages(),
+        )
