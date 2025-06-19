1db9965654c3c9a9536377dc3d128d7bc305405c
Sarah <sbetti@stsci.edu>
2025-06-19 15:23:09 -0400

    with packages

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from setuptools import setup, find_packages
 
-# setup(name="bdfit", 
-#         version = "1.0",
-#         author = "Sarah Betti",
-#         author_email = "sbetti@stsci.edu",
-#         url = "https://github.com/sbetti22/bdfit",
-#         packages = find_packages(),
-#         )
-
 setup(
     name='bdfit',
     version='1.0',
